d28a619737ea1e0a49ac6f07336cf6b9ee5175ae
neeraj <niraj.jha@gmail.com>
2017-12-13 00:01:44 -0500

    Adding setter method

--- src/main/java/com/javagrad/demo/entities/Greeting.java
+++ src/main/java/com/javagrad/demo/entities/Greeting.java
@@ -12,6 +12,10 @@ public class Greeting {
         return greeting;
     }
 
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
